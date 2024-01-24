fc4a4a97c65340abcb40ff08625bb326d6957bf2
Sitois <143238636+Sitois@users.noreply.github.com>
2024-01-24 13:27:54 +0100

    fixed default rpc

--- config_selfbot.py
+++ config_selfbot.py
@@ -26,7 +26,7 @@ deltime = 20
 #   rpc         #
 #################
 
-activity_name = "Nculear"
+activity_name = "Nuclear"
 activity_details = "Nuclear $B"
 activity_state = "Best Free $B !"
 status_activity = "Online"
@@ -35,8 +35,8 @@ afk = False
 streaming_url = "https://twitch.tv/twitch"
 activity_button_one = "Nuclear !"
 activity_button_one_answer = "https://github.com/Sitois/Nuclear"
-activity_button_two = "Star it !"
+activity_button_two = "Star it!"
 activity_button_two_answer = "https://github.com/Sitois/Nuclear"
 image_key = "mp:attachments/1135264530188992562/1198281648437993553/CIjvBOJ.png?ex=65be55bf&is=65abe0bf&hm=40a3c63ca07dfac28726eadae220a07412551a69deea021b73c24ae00933782e&=&format=webp&quality=lossless" # see &tuto
-application_id = ""
+application_id = "1"
 #################
