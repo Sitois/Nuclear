76965ba0ab891d4f482bc8a44aa3e9d67290306e
Sitois <143238636+Sitois@users.noreply.github.com>
2024-01-27 19:13:38 +0100

    uh

--- config_selfbot.py
+++ config_selfbot.py
@@ -47,5 +47,5 @@ activity_button_one_answer = "https://github.com/Sitois/Nuclear"
 activity_button_two = "Star it!"
 activity_button_two_answer = "https://github.com/Sitois/Nuclear"
 image_key = "mp:attachments/1135264530188992562/1198281648437993553/CIjvBOJ.png?ex=65be55bf&is=65abe0bf&hm=40a3c63ca07dfac28726eadae220a07412551a69deea021b73c24ae00933782e&=&format=webp&quality=lossless" # see &tuto
-application_id = "1"
+application_id = "1200865454319747142"
 #################
