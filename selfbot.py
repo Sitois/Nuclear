ae382b3c1c49e796f1f5a870b5b6718a3bfa0141
Sitois <143238636+Sitois@users.noreply.github.com>
2024-02-06 21:08:22 +0100

    Improvement: Better visual for snipe command

--- selfbot.py
+++ selfbot.py
@@ -250,7 +250,7 @@ async def voice(ctx):
     
 @bot.cmd(description="Snipe (all servers).")
 async def snipe(ctx):
-    await ctx.message.edit(f"{bot.user.deleted_messages[-1].author}: {bot.user.deleted_messages[-1]}")
+    await ctx.message.edit(f"🚮 __Last message deleted (all servers):__ `{bot.user.deleted_messages[-1].author}`: {bot.user.deleted_messages[-1]}")
 
 #############
 
